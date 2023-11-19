60e96abb06109e4ee6ad9197484ea976a23983b3
Asad Saeed <asadsaeed12225@gmail.com>
2023-11-19 12:16:19 +0500

    feat: Design Users and Products page

--- src/app/dashboard/products/page.jsx
+++ src/app/dashboard/products/page.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Link from "next/link";
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+};
 
-export default Products
+export default Products;
--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
 
-const Users = () => {
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const UsersPage = async () => {
+
+
   return (
-    <div>Users</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+            <td>Asad</td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination/>
+    </div>
+  );
+};
 
-export default Users
+export default UsersPage;
